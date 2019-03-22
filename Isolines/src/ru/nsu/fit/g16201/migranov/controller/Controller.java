21e152d59a7bae92ea767b1d3f73b75e9b0d9ad5
Denis Migranov <d.migranov@g.nsu.ru>
2019-03-22 16:40:12 +0700

    first isolines commit

---
+++ Isolines/src/ru/nsu/fit/g16201/migranov/controller/Controller.java
@@ -0,0 +1,9 @@
+package ru.nsu.fit.g16201.migranov.controller;
+
+import java.io.File;
+
+public class Controller {
+    public int loadFile(File file) {
+        return 0;
+    }
+}
---
+++ Isolines/src/ru/nsu/fit/g16201/migranov/view/IsolinesFrame.java
@@ -0,0 +1,161 @@
+package ru.nsu.fit.g16201.migranov.view;
+
+import ru.nsu.fit.g16201.migranov.controller.Controller;
+import ru.nsu.fit.g16201.migranov.view.frametemplate.MainFrame;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.lang.reflect.Method;
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IsolinesFrame extends MainFrame {
+    private JLabel statusLabel = new JLabel("");
+
+    private List<AbstractButton> deactivatedButtons = new ArrayList<>();
+    private Controller controller;
+
+    public static void main(String[] args) throws Exception {
+        new IsolinesFrame();
+    }
+
+    private IsolinesFrame() throws Exception {
+        super(800, 600, "Untitled | Denis Migranov, 16201");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        controller = new Controller();
+
+
+
+        addMenus();
+
+
+        setVisible(true);
+    }
+
+    private void addMenus() throws NoSuchMethodException {
+        addSubMenu("File", KeyEvent.VK_F);
+        addMenuAndToolBarButton("File/Open", "Open a file", KeyEvent.VK_O, "upload-1.png", "onOpen", false);
+        addMenuAndToolBarButton("File/Exit", "Exit the aplication", KeyEvent.VK_E, "logout.png", "onExit", false);
+
+    }
+
+    private void addMenuAndToolBarButton(String path, String tooltip, int mnemonic, String icon, String actionMethod, boolean isDeactivated) throws NoSuchMethodException
+    {
+        MenuElement element = getParentMenuElement(path);
+        if(element == null)
+            throw new InvalidParameterException("Menu path not found: " + path);
+        String title = getMenuPathName(path);
+        JMenuItem item = new JMenuItem(title);
+        item.setMnemonic(mnemonic);
+        item.setToolTipText(tooltip);
+        item.addMouseListener(new StatusTitleListener(statusLabel));
+        final Method method = getClass().getMethod(actionMethod);
+        item.addActionListener(evt -> {
+            try {
+                method.invoke(IsolinesFrame.this);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        if(element instanceof JMenu)
+            ((JMenu)element).add(item);
+        else if(element instanceof JPopupMenu)
+            ((JPopupMenu)element).add(item);
+        else
+            throw new InvalidParameterException("Invalid menu path: " + path);
+
+        JButton button = new JButton();
+        if(icon != null)
+            button.setIcon(new ImageIcon(getClass().getResource("resources/"+icon), title));
+        for(ActionListener listener: item.getActionListeners())
+            button.addActionListener(listener);
+        button.setToolTipText(tooltip);
+        button.addMouseListener(new StatusTitleListener(statusLabel));
+        toolBar.add(button);
+        if(isDeactivated)
+        {
+            item.setEnabled(false);
+            button.setEnabled(false);
+            deactivatedButtons.add(item);
+            deactivatedButtons.add(button);
+        }
+    }
+
+    private void addCheckBoxMenuAndToolBarButton(String path, String tooltip, int mnemonic, String icon, String actionMethod, boolean state, boolean isDeactivated) throws NoSuchMethodException
+    {
+        MenuElement element = getParentMenuElement(path);
+        if(element == null)
+            throw new InvalidParameterException("Menu path not found: " + path);
+        String title = getMenuPathName(path);
+
+        JCheckBoxMenuItem item = new JCheckBoxMenuItem(title, state);
+
+        item.setMnemonic(mnemonic);
+        item.setToolTipText(tooltip);
+
+        item.addMouseListener(new StatusTitleListener(statusLabel));
+
+        final Method method = getClass().getMethod(actionMethod);
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                try {
+                    method.invoke(IsolinesFrame.this);
+                } catch (Exception e) {
+                    //throw new RuntimeException(e);
+                }
+            }
+        });
+        if(element instanceof JMenu)
+            ((JMenu)element).add(item);
+        else if(element instanceof JPopupMenu)
+            ((JPopupMenu)element).add(item);
+        else
+            throw new InvalidParameterException("Invalid menu path: " + path);
+
+        JToggleButton button = new JToggleButton();
+        if(icon != null)
+            button.setIcon(new ImageIcon(getClass().getResource("resources/"+icon), title));
+        button.setToolTipText(tooltip);
+        button.setModel(item.getModel());   //кнопки повторяют поведение меню, включая "зажатость"
+        toolBar.add(button);
+        button.addMouseListener(new StatusTitleListener(statusLabel));
+
+
+        if(isDeactivated)
+        {
+            item.setEnabled(false);
+            button.setEnabled(false);
+            deactivatedButtons.add(item);
+            deactivatedButtons.add(button);
+        }
+    }
+
+    public void onOpen()
+    {
+        File file = getOpenFileName("txt", "A function description file");
+        if(file != null) {
+            setTitle(file.getName() + " | Denis Migranov, 16201");
+            int r = controller.loadFile(file);
+            if(r == 0)
+            {
+                for (AbstractButton b : deactivatedButtons)
+                {
+                    b.setEnabled(true);
+                }
+            }
+        }
+    }
+
+    public void onExit()
+    {
+        System.exit(0);
+    }
+
+
+}
