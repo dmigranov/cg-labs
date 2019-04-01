f8dcb30a492879f48e210fbf6f0bf9a527224b38
Denis Migranov <d.migranov@g.nsu.ru>
2019-04-01 20:06:09 +0700

    gonna do something

--- Isolines/src/ru/nsu/fit/g16201/migranov/controller/Seed.java
+++ Isolines/src/ru/nsu/fit/g16201/migranov/controller/Seed.java
@@ -3,14 +3,24 @@ package ru.nsu.fit.g16201.migranov.controller;
 import java.awt.*;
 
 class Seed {
+    private static final int LEFT = 1;
+    private static final int RIGHT = 2;
+    private static final int UP = 4;
+    private static final int DOWN = 8;
+
+
+
+
     int color;
+    int direction;
     double x;
     double y;
 
-    Seed(Color color, double x, double y)
+    Seed(Color color, double x, double y, int direction)
     {
         this.color = color.getRGB();
         this.x = x; this.y = y;
+        this.direction = direction;
     }
 
     @Override
