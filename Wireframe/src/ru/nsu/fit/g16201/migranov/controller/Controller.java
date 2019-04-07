677d3be50fce98edac9f467db0bddb5a0cd495d9
Denis Migranov <d.migranov@g.nsu.ru>
2019-04-07 14:06:44 +0700

    cfcghfgfgh

--- Wireframe/src/ru/nsu/fit/g16201/migranov/controller/Controller.java
+++ Wireframe/src/ru/nsu/fit/g16201/migranov/controller/Controller.java
@@ -1,6 +1,8 @@
 package ru.nsu.fit.g16201.migranov.controller;
 
+import ru.nsu.fit.g16201.migranov.model.Figure;
 import ru.nsu.fit.g16201.migranov.model.Matrix;
+import ru.nsu.fit.g16201.migranov.model.Point2D;
 import ru.nsu.fit.g16201.migranov.model.Point3D;
 
 import java.awt.*;
@@ -8,6 +10,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Controller {
     private int n, m, k;
@@ -40,13 +44,8 @@ public class Controller {
             sw = Double.parseDouble(substrings[2]);
             sh = Double.parseDouble(substrings[3]);
 
-            /*Matrix sceneRotateMatrix = new Matrix(3, 3);
-            for(int i = 0; i < 3; i++)
-            {
-                substrings = readLineAndSplit();
-                sceneRotateMatrix.setRow(i, new double[] {Double.parseDouble(substrings[0]), Double.parseDouble(substrings[1]), Double.parseDouble(substrings[2])});
-            }*/
             sceneRotateMatrix = readMatrixByRow(3, 3);
+            //todo: дополнить до 4x4?
 
             substrings = readLineAndSplit();
             backgroundColor = new Color(Integer.parseInt(substrings[0]), Integer.parseInt(substrings[1]), Integer.parseInt(substrings[2]));
@@ -64,13 +63,18 @@ public class Controller {
                 Point3D center = new Point3D(Double.parseDouble(substrings[0]), Double.parseDouble(substrings[1]), Double.parseDouble(substrings[2]));
 
                 Matrix rotateMatrix = readMatrixByRow(3, 3);
+                //todo: дополнить до 4x4?
 
                 substrings = readLineAndSplit();
                 int splinePointCount = Integer.parseInt(substrings[0]);
+                List<Point2D> splinePoints = new ArrayList<>();
                 for(int j = 0; j < splinePointCount; j++)
                 {
-
+                    substrings = readLineAndSplit();
+                    Point2D splinePoint = new Point2D(Double.parseDouble(substrings[0]), Double.parseDouble(substrings[1]));
+                    splinePoints.add(splinePoint);
                 }
+                Figure figure  = new Figure(center, color, rotateMatrix, splinePoints);
 
             }
 
--- Wireframe/src/ru/nsu/fit/g16201/migranov/model/Figure.java
+++ Wireframe/src/ru/nsu/fit/g16201/migranov/model/Figure.java
@@ -1,12 +1,19 @@
 package ru.nsu.fit.g16201.migranov.model;
 
 import java.awt.*;
+import java.util.List;
 
 public class Figure {
     private Color color;
     private Point3D center;
-
     private Matrix rotateMatrix;
+    private List<Point2D> splinePoints;
 
 
+    public Figure(Point3D center, Color color, Matrix rotateMatrix, List<Point2D> splinePoints) {
+        this.center = center;
+        this.color = color;
+        this.rotateMatrix = rotateMatrix;
+        this.splinePoints = splinePoints;
+    }
 }
