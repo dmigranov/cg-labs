8e2059e4bace9fd1297be682aa1f680b8a41c9c3
Денис  Мигранов <d.migranov@g.nsu.ru>
2019-04-17 13:16:13 +0700

    xczcxzcxzc

---
+++ Wireframe/src/ru/nsu/fit/g16201/migranov/view/LabelTextField.java
@@ -0,0 +1,12 @@
+package ru.nsu.fit.g16201.migranov.view;
+
+import javax.swing.*;
+import java.awt.event.KeyListener;
+
+public class LabelTextField extends JPanel {
+    LabelTextField(String s, JTextField textField, KeyListener listener)
+    {
+
+    }
+
+}
