2e67c1b9075e01f9632bd575f4392f017430c9a0
Denis Migranov <d.migranov@g.nsu.ru>
2019-04-27 19:49:36 +0700

    started doing interface

--- Raytracing/src/ru/nsu/fit/g16201/migranov/view/RaytracingFrame.java
+++ Raytracing/src/ru/nsu/fit/g16201/migranov/view/RaytracingFrame.java
@@ -3,6 +3,12 @@ package ru.nsu.fit.g16201.migranov.view;
 import ru.nsu.fit.g16201.migranov.view.frametemplate.MainFrame;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.lang.reflect.Method;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,9 +25,83 @@ public class RaytracingFrame extends MainFrame {
 
         addMenus();
 
+        JPanel statusPanel = new JPanel();
+        statusPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        statusPanel.setPreferredSize(new Dimension(getWidth(), 16));
+        statusLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        statusPanel.setLayout(new BoxLayout(statusPanel, BoxLayout.X_AXIS));
+        statusPanel.add(statusLabel);
+        add(statusPanel, BorderLayout.SOUTH);
+
+        setMinimumSize(new Dimension(800, 600));
+        setLocationRelativeTo(null);
+        setVisible(true);
+
     }
 
-    private void addMenus() {
+    private void addMenus() throws NoSuchMethodException{
+        addSubMenu("File", KeyEvent.VK_F);
+        addMenuAndToolBarButton("File/Open", "Load a scene file", KeyEvent.VK_O, "upload-1.png", "onOpen", false);
+        addMenuAndToolBarButton("File/Load render settings", "Load render settings from file", KeyEvent.VK_R, "upload.png", "onOpenRenderSettings", true);
+        addMenuAndToolBarButton("File/Save render settings as", "Save render settings to specified file", KeyEvent.VK_S, "download.png", "onSaveRenderSettings", true);
 
+    }
+
+    public void onOpenRenderSettings()
+    {
+
+    }
+
+    public void onOpen()
+    {
+
+    }
+
+    public void onSaveRenderSettings()
+    {
+
+    }
+
+    private void addMenuAndToolBarButton(String path, String tooltip, int mnemonic, String icon, String actionMethod, boolean isDeactivated) throws NoSuchMethodException
+    {
+        MenuElement element = getParentMenuElement(path);
+        if(element == null)
+            throw new InvalidParameterException("Menu path not found: " + path);
+        String title = getMenuPathName(path);
+        JMenuItem item = new JMenuItem(title);
+        item.setMnemonic(mnemonic);
+        item.setToolTipText(tooltip);
+        item.addMouseListener(new StatusTitleListener(statusLabel));
+        final Method method = getClass().getMethod(actionMethod);
+        item.addActionListener(evt -> {
+            try {
+                method.invoke(RaytracingFrame.this);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        if(element instanceof JMenu)
+            ((JMenu)element).add(item);
+        else if(element instanceof JPopupMenu)
+            ((JPopupMenu)element).add(item);
+        else
+            throw new InvalidParameterException("Invalid menu path: " + path);
+
+        JButton button = new JButton();
+        if(icon != null)
+            button.setIcon(new ImageIcon(getClass().getResource("resources/"+icon), title));
+        for(ActionListener listener: item.getActionListeners())
+            button.addActionListener(listener);
+        button.setToolTipText(tooltip);
+        button.addMouseListener(new StatusTitleListener(statusLabel));
+        toolBar.add(button);
+        if(isDeactivated)
+        {
+            item.setEnabled(false);
+            button.setEnabled(false);
+            deactivatedButtons.add(item);
+            deactivatedButtons.add(button);
+        }
     }
 }
